99fcce1c476b59cecf047724842ee75716c53e8b
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-02-27 09:36:35 -0300

    Branch principal do projeto

---
+++ src/Principal/Principal.java
@@ -0,0 +1,30 @@
+package Principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static Scanner leia = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao, id, quantidade;
+		float valor;
+		String nome;
+		
+		while (true) {
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+	}
+}
